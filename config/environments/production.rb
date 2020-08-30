c963d46939eacaf8240c008d99af274231f5128d
Ramu <ramkumar.ramu1@gmail.com>
2020-08-30 21:19:23 +0530

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,6 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-  config.serve_static_assets = true
-  config.assets.digest = true
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
