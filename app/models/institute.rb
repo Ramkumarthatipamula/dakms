4deed2010d0ddd2b103542311c1e5bf172271bfa
Ramu <ramkumar.ramu1@gmail.com>
2020-08-31 03:05:33 +0530

    added validation

--- app/models/institute.rb
+++ app/models/institute.rb
@@ -1,8 +1,8 @@
 class Institute < User
-  validates :name, presence: true
   validates :institute_centre_name, presence: true
   validates :institute_centre_type, presence: true
   validates :location, presence: true
   validates :address, presence: true
   validates :mobile_number, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
