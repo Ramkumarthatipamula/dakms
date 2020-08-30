dec891ec30e43001a7ad2e544953c52e3176637b
Ramu <ramkumar.ramu1@gmail.com>
2020-08-30 23:48:58 +0530

    view adjustmensts

--- app/controllers/institutes_controller.rb
+++ app/controllers/institutes_controller.rb
@@ -4,7 +4,16 @@ class InstitutesController < ApplicationController
   # GET /institutes
   # GET /institutes.json
   def index
-    @institutes = Institute.all
+    # @institutes = Institute.all
+    if current_user.type == 'Admin'
+      redirect_to admin_path
+    elsif current_user.type == 'ElectronicWing'
+      redirect_to electronic_wing_dashboard_path
+    elsif current_user.type == 'Institute'
+      redirect_to institute_dashboard_path
+    elsif current_user.type == 'SuperAdmin'
+      redirect_to super_admin_path
+    end
   end
 
   # GET /institutes/1
