0346c92aa1041b6f6c5816c6a3ead046bf3b1cc6
Ramu <ramkumar.ramu1@gmail.com>
2020-09-13 02:10:58 +0530

    migration file deleted

--- db/migrate/20200825152424_add_fields_to_user.rb
+++ db/migrate/20200825152424_add_fields_to_user.rb
@@ -1,6 +1,5 @@
 class AddFieldsToUser < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :institute_type, :string
     add_column :users, :institute_centre_name, :string
     add_column :users, :institute_centre_type, :string
   end
