af09e130f2acf2434e7cc987872484b51d73fa6b
Ramu <ramkumar.ramu1@gmail.com>
2020-09-15 23:56:56 +0530

    seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-user1 = User.new(discipline: "Research", institute_centre_type: 'RARS', institute_centre_name: 'Rajendranagar',location: "Rajendranagar", address: "500030", mobile_number: "9014488000", type: "Institute", name: "Institute", email: "institute@demo.com", password: "000000", pwd: "000000")
-user1.save(validate: false)
+# user1 = User.new(discipline: "Research", institute_centre_type: 'RARS', institute_centre_name: 'Rajendranagar',location: "Rajendranagar", address: "500030", mobile_number: "9014488000", type: "Institute", name: "Institute", email: "institute@demo.com", password: "000000", pwd: "000000")
+# user1.save(validate: false)
+User.destroy_all
+UploadFile.destroy_all
+
+p "Destroyed all data"
+p "seed started..."
 user2 = User.new(type: "ElectronicWing", name: "electronic_wing", email: "electronic_wing@demo.com", password: "password@ew", pwd: "password@ew")
 user2.save(validate: false)
-user3 = User.new(discipline: "Teaching", institute_centre_type: 'UG/PG Colleges', institute_centre_name: 'Palem',location: "Palem", address: "500030", mobile_number: "9014488000", type: "Institute", name: "Teaching", email: "teaching@demo.com", password: "000000", pwd: "000000")
-user3.save(validate: false)
+# user3 = User.new(discipline: "Teaching", institute_centre_type: 'UG/PG Colleges', institute_centre_name: 'Palem',location: "Palem", address: "500030", mobile_number: "9014488000", type: "Institute", name: "Teaching", email: "teaching@demo.com", password: "000000", pwd: "000000")
+# user3.save(validate: false)
+
+require 'csv'
+
+file = "#{Rails.root}/public/import_institutes.csv"
+csv_text = File.read(file)
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  p row.to_hash[:type] = "Institute"
+  p row
+  user = Institute.new(row.to_hash)
+  user.save
+end
+
 p "completed"
